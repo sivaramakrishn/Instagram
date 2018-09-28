59be59823cc10d55b609d553dd3edc0353d5470b
sivaramakrishn <43650271+sivaramakrishn@users.noreply.github.com>
2018-09-28 23:34:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import AddPost from "./Components/AddPost";
+import PostFeed from "./Components/Postfeed/PostFeed";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import loader from "./images/loader.gif";
+import { connect } from "react-redux";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          {this.props.getPostReducer.loading && (
+            <div className="loader-div">
+              <img src={loader} className="loader" />
+            </div>
+          )}
+          {this.props.addPostReducer.loading && (
+            <div className="loader-div">
+              <img src={loader} className="loader" />
+            </div>
+          )}
+          <Route exact path="/" component={PostFeed} />
+          <Route exact path="/add" component={AddPost} />
+        </div>
+      </Router>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return state;
+};
+export default connect(mapStateToProps)(App);
